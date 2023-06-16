f23b51f106a899b0e886ff6c3f05c33bc78ac0df
hiroto2002 <hiroto2002h14@icloud.com>
2023-06-16 18:50:45 +0900

    fix:layout "use client" bug

--- frontend/src/components/layouts/header/Header.tsx
+++ frontend/src/components/layouts/header/Header.tsx
@@ -1,88 +1,95 @@
-import { Styles } from "@/types/styles"
-import React, { useEffect, useState } from "react"
-import { Text } from "../../elements/text/Text"
-import Link from "next/link"
-import { Flex } from "@/components/elements/box/Flex"
-import Image from "next/image"
+"use client";
 
-type Props = {
-  isActive: (path: string) => boolean
-}
+import { Styles } from "@/types/styles";
+import React, { useEffect, useState } from "react";
+import { Text } from "../../elements/text/Text";
+import Link from "next/link";
+import { Flex } from "@/components/elements/box/Flex";
+import Image from "next/image";
+import { useCustomRouter } from "@/hooks/useCustomRouter";
 
-export const Header = (props: Props) => {
-  const [selectedButton, setSelectedButton] = useState("/")
+export const Header = () => {
+  const { isActive } = useCustomRouter();
+  // hookで用意してる
+  // const [selectedButton, setSelectedButton] = useState("/")
 
-  const handleActivate = (path: string) => {
-    setSelectedButton(path)
-  }
+  // const handleActivate = (path: string) => {
+  //   setSelectedButton(path)
+  // }
 
-  const [isScrolled, setIsScrolled] = useState(false)
+  const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 90) {
-        setIsScrolled(true)
-      }else {
-        setIsScrolled(false)
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
       }
-    }
-    window.addEventListener("scroll", handleScroll)
+    };
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  })
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
 
-  const buttonStyle = isScrolled ? styles.scrolled_button : styles.button
-  const selectedButtonStyle = isScrolled ? styles.scrolled_selectedButton : styles.selectedButton
-  
+  const buttonStyle = isScrolled ? styles.scrolled_button : styles.button;
+  const selectedButtonStyle = isScrolled
+    ? styles.scrolled_selectedButton
+    : styles.selectedButton;
 
   const BUTTON = [
     {
       name: "ホーム",
       sub: "Home",
-      icon_path: '/header/home.svg',
+      icon_path: "/header/home.svg",
       path: "/",
     },
     {
       name: "上映スケジュール",
       sub: "Schedule",
-      icon_path: '/header/schedule.svg',
+      icon_path: "/header/schedule.svg",
       path: "/schedule",
     },
     {
       name: "施設案内",
       sub: "Theater",
-      icon_path: '/header/theater.svg',
+      icon_path: "/header/theater.svg",
       path: "/theater",
     },
     {
       name: "作品案内",
       sub: "Movies",
-      icon_path: '/header/movie.svg',
+      icon_path: "/header/movie.svg",
       path: "/movie",
     },
     {
       name: "料金案内",
       sub: "Price",
-      icon_path: '/header/price.svg',
+      icon_path: "/header/price.svg",
       path: "/price",
     },
-  ]
+  ];
   return (
-    <Flex style={!isScrolled ? styles.container : styles.scrolled} direction="column">
+    <Flex
+      style={!isScrolled ? styles.container : styles.scrolled}
+      direction="column"
+    >
       <Flex style={styles.top_nav} justify="space-between" align="center">
         <Link href="/" style={styles.logo}>
           HAL<Text>CINEMA</Text>
         </Link>
         <Flex direction="row" style={styles.settings_nav}>
-          <Link style={styles.faq} href="/FAQ">？</Link>
+          <Link style={styles.faq} href="/FAQ">
+            ？
+          </Link>
           <Link style={styles.lang} href="/login">
             <Image
-              src={'/header/language.svg'}
+              src={"/header/language.svg"}
               width={18}
               height={18}
-              alt='ʕ◔ϖ◔ʔ'
-              style={{marginRight: '10px'}}
+              alt="ʕ◔ϖ◔ʔ"
+              style={{ marginRight: "10px" }}
             />
             言語選択
           </Link>
@@ -90,21 +97,23 @@ export const Header = (props: Props) => {
       </Flex>
       <Flex style={!isScrolled ? styles.bottom_nav : styles.scrolled_nav}>
         {BUTTON.map((button, index) => (
-          <Link href={button.path} key={index} onClick={()=>handleActivate(button.path)}>
+          <Link href={button.path} key={index}>
             <Flex
-              style={button.path === selectedButton ? selectedButtonStyle : buttonStyle }
+              style={isActive(button.path) ? selectedButtonStyle : buttonStyle}
               direction="column"
               justify="center"
               align="center"
             >
-              {!isScrolled &&
-              <Image
-                src={button.icon_path}
-                width={40}
-                height={40}
-                alt="ʕ◔ϖ◔ʔ"
-                style={{marginBottom: '10px'}}
-              />}
+              {!isScrolled && (
+                <Image
+                  src={button.icon_path}
+                  width={40}
+                  height={40}
+                  // ↓かわいい
+                  alt="ʕ◔ϖ◔ʔ"
+                  style={{ marginBottom: "10px" }}
+                />
+              )}
               {button.name}
             </Flex>
           </Link>
@@ -131,93 +140,93 @@ const styles: Styles = {
   },
   top_nav: {
     width: "100%",
-    height: '100px',
+    height: "100px",
     color: "#eee",
   },
   logo: {
-    padding: '10px',
+    padding: "10px",
     fontSize: "30px",
     color: "#eee",
   },
   settings_nav: {
-    margin: 'auto 0',
+    margin: "auto 0",
   },
   faq: {
     display: "flex",
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     color: "#666",
     background: "#eee",
-    fontWeight: 'bolder',
-    width: '50px',
-    height: '50px',
-    marginRight: '30px',
-    fontSize: '1.2rem',
-    borderRadius: '100%',
+    fontWeight: "bolder",
+    width: "50px",
+    height: "50px",
+    marginRight: "30px",
+    fontSize: "1.2rem",
+    borderRadius: "100%",
   },
   lang: {
     display: "flex",
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     color: "#666",
     background: "#eee",
-    fontWeight: 'bolder',
-    width: '150px',
-    height: '50px',
-    marginRight: '30px',
-    borderRadius: '5px',
+    fontWeight: "bolder",
+    width: "150px",
+    height: "50px",
+    marginRight: "30px",
+    borderRadius: "5px",
   },
   bottom_nav: {
     width: "100%",
-    height: '120px',
+    height: "120px",
     display: "flex",
-    alignItems: 'end',
+    alignItems: "end",
     justifyContent: "center",
     backgroundColor: "#eee",
-    boxShadow: '0 0 30px #555',
+    boxShadow: "0 0 30px #555",
   },
   scrolled_nav: {
     width: "100%",
-    height: '50px',
+    height: "50px",
     display: "flex",
     justifyContent: "center",
     backgroundColor: "#eee",
-    boxShadow: '0 0 30px #555',
+    boxShadow: "0 0 30px #555",
   },
   button: {
     color: "#333",
-    width: '202px',
-    height: '110px',
-    fontSize: '15px',
-    fontWeight: 'bold',
+    width: "202px",
+    height: "110px",
+    fontSize: "15px",
+    fontWeight: "bold",
     padding: "10px 20px 0 20px",
   },
   scrolled_button: {
     color: "#333",
-    width: '202px',
-    height: '50px',
-    fontSize: '15px',
-    fontWeight: 'bold',
+    width: "202px",
+    height: "50px",
+    fontSize: "15px",
+    fontWeight: "bold",
     padding: "10px 20px 0 20px",
   },
-  selectedButton : {
-    backgroundColor: '#CDD0EF',
+  selectedButton: {
+    backgroundColor: "#CDD0EF",
     color: "#333",
-    width: '202px',
-    height: '110px',
-    borderRadius: '15px 15px 0 0',
-    fontSize: '15px',
-    fontWeight: 'bold',
+    width: "202px",
+    height: "110px",
+    borderRadius: "15px 15px 0 0",
+    fontSize: "15px",
+    fontWeight: "bold",
     padding: "10px 20px 0 20px",
   },
   scrolled_selectedButton: {
-    backgroundColor: '#CDD0EF',
+    backgroundColor: "#CDD0EF",
     color: "#333",
-    width: '202px',
-    height: '65px',
-    borderRadius: '0 0 15px 15px',
-    fontSize: '15px',
-    fontWeight: 'bold',
+    width: "202px",
+    height: "65px",
+    borderRadius: "0 0 15px 15px",
+    fontSize: "15px",
+    fontWeight: "bold",
     padding: "10px 20px 0 20px",
   },
 };
