d6fcff9e1269157b3bf1ac286619dc1ce4dfdc25
aijkl <aijkl@outlook.jp>
2023-06-21 19:45:18 +0900

    add: ページングを追加

--- backend/HalCinema/Api/Controllers/MoviesController.cs
+++ backend/HalCinema/Api/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 using Api.Models;
 using Database;
+using Database.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,14 +11,22 @@ namespace Api.Controllers;
 public class MoviesController : ControllerBase
 {
     private readonly CinemaContext _context;
+    private const int Limit = 10;
     public MoviesController(CinemaContext context)
     {
         _context = context;
     }
 
     [HttpGet]
-    public ActionResult<Response> Get()
+    public async Task<ActionResult<PageableResponse>> Get(int? nextPageToken)
     {
-        return new Response(true, null, _context.Movies.Include(x => x.Genres).AsNoTracking().ToList());
+        IQueryable<Movie> query = _context.Movies;
+        
+        if (nextPageToken.HasValue)
+        {
+            query = query.Where(item => item.Id > nextPageToken);
+        }
+        var items = await query.OrderBy(item => item.Id).Take(Limit).ToListAsync();
+        return new PageableResponse(items.Count == Limit ? items.Last().Id : null, null, items );
     }
 }
--- backend/HalCinema/Api/Models/Response.cs
+++ backend/HalCinema/Api/Models/Response.cs
@@ -1,3 +1,4 @@
 namespace Api.Models;
 
 public record Response(bool Success, string? Message, object? Data);
+public record PageableResponse(int? NextPageToken, string? Message, object? Data) : Response(true, Message, Data);
