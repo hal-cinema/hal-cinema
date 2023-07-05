94458fccc0196cc976572bf4b02f414b477bb9a9
coa3 <lovecoal928@gmail.com>
2023-07-05 16:56:03 +0900

    feat: progressGuide

--- frontend/src/app/reserve/page.tsx
+++ frontend/src/app/reserve/page.tsx
@@ -1,9 +1,31 @@
+import { Flex } from '@/components/elements/box/Flex'
+import { Header } from '@/components/layouts/header/Header'
+import ProgressGuide from '@/components/organism/reserve/ProgressGuide'
+import SeatsSelect from '@/components/organism/reserve/SeatsSelect'
+import TicketCalc from '@/components/organism/reserve/TicketCalc'
+import { Styles } from '@/types/styles'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>reserve page</div>
+    <>
+      <Header/>
+      <Flex style={styles.container} direction="column" align="center">
+        <ProgressGuide />
+        <Flex direction='row' align='center'>
+          <TicketCalc />
+          <SeatsSelect />
+        </Flex>
+      </Flex>
+    </>
   )
+}
+
+const styles: Styles = {
+  container: {
+    width: "100%",
+    background: "#fff",
+  },
 }
 
 export default page
