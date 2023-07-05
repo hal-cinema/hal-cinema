b4d05525065d2d7d16373ea8eb4d671fb5fee3e3
minntoko <minntoko0822@gmail.com>
2023-07-05 22:29:40 +0900

    add: 日付を選択で上映スケジュールを表示

--- frontend/src/app/schedule/page.tsx
+++ frontend/src/app/schedule/page.tsx
@@ -13,14 +13,693 @@ import "swiper/css/navigation";
 import { Navigation } from "swiper";
 
 const Schedule = () => {
+
   const weeks = useSchewdule();
 
-  const [active, setActive] = useState(0);
+  type ScreenSchedule = {
+    screen: string;
+    startTime: string;
+    endTime: string;
+    sale: string;
+  };
+  type MovieSchedule = {
+    title: string;
+    time: number;
+    imageURL: string;
+    screenSchedules: ScreenSchedule[];
+  };
+  type DailySchedule = MovieSchedule[];
+  type WeeklySchedule = DailySchedule[];
+
+  const weeklySchedule: WeeklySchedule = [
+    [
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+      {
+        title: "字幕 ワイルド・スピード／ファイヤーブースト",
+        time: 155,
+        imageURL: "/speedMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 5",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: "字幕 ワイルド・スピード／ファイヤーブースト",
+        time: 155,
+        imageURL: "/speedMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 5",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+        ],
+      },
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenSold",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenSold",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenSold",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 4",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 5",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 6",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: "字幕 ワイルド・スピード／ファイヤーブースト",
+        time: 155,
+        imageURL: "/speedMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 5",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenSold",
+          },
+          {
+            screen: "Screen 6",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 4",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 7",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 6",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenLow",
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: "字幕 ワイルド・スピード／ファイヤーブースト",
+        time: 155,
+        imageURL: "/speedMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+        ],
+      },
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 2",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 7",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+          {
+            screen: "Screen 5",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: "字幕 ワイルド・スピード／ファイヤーブースト",
+        time: 155,
+        imageURL: "/speedMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+        ],
+      },
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: "字幕 ワイルド・スピード／ファイヤーブースト",
+        time: 155,
+        imageURL: "/speedMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenSold",
+          },
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+        ],
+      },
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenSold",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 6",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenLow",
+          },
+        ],
+      },
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: "字幕 ワイルド・スピード／ファイヤーブースト",
+        time: 155,
+        imageURL: "/speedMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 1",
+            startTime: "15:00",
+            endTime: "16:50",
+            sale: "screenOnSale",
+          },
+        ],
+      },
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 3",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 2",
+            startTime: "12:00",
+            endTime: "13:50",
+            sale: "screenLow",
+          },
+          {
+            screen: "Screen 3",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenLow",
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: "字幕 ワイルド・スピード／ファイヤーブースト",
+        time: 155,
+        imageURL: "/speedMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 4",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+      {
+        title: "ザ・スーパーマリオブラザーズ・ムービー",
+        time: 110,
+        imageURL: "/marioMovie.png",
+        screenSchedules: [
+          {
+            screen: "Screen 1",
+            startTime: "10:00",
+            endTime: "11:50",
+            sale: "screenOnSale",
+          },
+          {
+            screen: "Screen 4",
+            startTime: "14:00",
+            endTime: "15:50",
+            sale: "screenSold",
+          },
+        ],
+      },
+    ],
+  ];
+  
+  const [itemOffset, setItemOffset] = useState(0);
+  
+  const todaySchedule = weeklySchedule[itemOffset];
+
+  console.log(todaySchedule);
+  
 
   const handleDateClick = (e: MouseEvent<HTMLDivElement>) => {
     const { index } = e.currentTarget.dataset;
-    console.log(`${index}がクリックされた`);
-    setActive(+index!);
+    setItemOffset(+index!);
   };
 
   return (
@@ -48,7 +727,11 @@ const Schedule = () => {
           >
             {weeks.map((week, index) => (
               <SwiperSlide
-                className={`${scheduleStyle.day} ${active === index ? scheduleStyle.active : scheduleStyle.disactive}`}
+                className={`${scheduleStyle.day} ${
+                  itemOffset === index
+                    ? scheduleStyle.active
+                    : scheduleStyle.disactive
+                }`}
                 key={week.date}
                 data-index={index}
                 onClick={handleDateClick}
@@ -63,10 +746,17 @@ const Schedule = () => {
           </div>
         </div>
         {/* 映画スケジュールコンポーネント */}
-        <Movie title="ザ・スーパーマリオブラザーズ・ムービー" time={110} imageURL="/marioMovie.png" />
-        <Movie title="字幕 ワイルド・スピード／ファイヤーブースト" time={155} imageURL="/speedMovie.png" />
-        <Movie title="ザ・スーパーマリオブラザーズ・ムービー" time={110} imageURL="/marioMovie.png" />
-        <Movie title="字幕 ワイルド・スピード／ファイヤーブースト" time={155} imageURL="/speedMovie.png" />
+        {
+          todaySchedule.map((movieSchedule, index) => {
+            return(
+            <Movie
+              key={index}
+              title={movieSchedule.title}
+              time={movieSchedule.time}
+              imageURL={movieSchedule.imageURL}
+              screenSchedules={movieSchedule.screenSchedules}
+            />)
+          })}
       </div>
     </>
   );
