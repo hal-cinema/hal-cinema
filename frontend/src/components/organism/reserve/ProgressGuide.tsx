596c918cac2d0a8126bf67c1b6f375433f40af8c
coa3 <lovecoal928@gmail.com>
2023-07-07 00:30:51 +0900

    feat: recoilで進捗バーの状態管理

--- frontend/src/components/organism/reserve/ProgressGuide.tsx
+++ frontend/src/components/organism/reserve/ProgressGuide.tsx
@@ -1,17 +1,55 @@
+'use client'
 import { Flex } from '@/components/elements/box/Flex'
-import ProgressBox from '@/components/elements/box/ProgressBox'
+import ProgressBox from '@/components/organism/reserve/ProgressBox'
 import { Styles } from '@/types/styles'
 import React from 'react'
+import { IsTicketsState } from '@/hooks/reserve/useProgressState'
+import { useRecoilValue } from 'recoil'
+
+type Progress = {
+  label: string
+  isProgress: boolean
+}
 
 const ProgressGuide = () => {
+  const PROGRESS: Progress[] = [
+    {
+      label: "券種選択",
+      isProgress: true
+    },
+    {
+      label: "座席指定",
+      isProgress: false
+    },
+    {
+      label: "お客様情報入力",
+      isProgress: false
+    },
+    {
+      label: "決済情報入力",
+      isProgress: false
+    },
+    {
+      label: "完了",
+      isProgress: false
+    },
+  ]
+
+  const selectTickets = useRecoilValue(IsTicketsState)
+  if(selectTickets){
+    PROGRESS[0].isProgress = true
+    PROGRESS[1].isProgress = false
+  }else if(!selectTickets){
+    PROGRESS[0].isProgress = false
+    PROGRESS[1].isProgress = true
+  }
+
   return (
     <>
       <Flex direction='row' align='center'>
-        <ProgressBox label={"券種選択"} isProgress={true} />
-        <ProgressBox label={"座席指定"} isProgress={false} />
-        <ProgressBox label={"お客様情報入力"} isProgress={false} />
-        <ProgressBox label={"決済情報入力"} isProgress={false} />
-        <ProgressBox label={"完了"} isProgress={false} />
+        {PROGRESS.map((progress, index) => (
+          <ProgressBox key={index} label={progress.label} isProgress={progress.isProgress} />
+        ))}
       </Flex>
     </>
   )
