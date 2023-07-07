863f4b3345dadb83ed54e2c8930d52742271650b
AisyA72 <b72-2002-arceus@docomo.ne.jp>
2023-07-07 17:43:46 +0900

    feat: 見出しのデザイン変更

--- frontend/src/components/organism/theater/ContentTitle.tsx
+++ frontend/src/components/organism/theater/ContentTitle.tsx
@@ -1,6 +1,6 @@
-import { Flex } from '@/components/elements/box/Flex'
-import { Styles } from '@/types/styles'
-import React from 'react'
+import { Flex } from "@/components/elements/box/Flex";
+import { Styles } from "@/types/styles";
+import React from "react";
 
 type Props = {
   label: string;
@@ -8,24 +8,24 @@ type Props = {
 const ContentTitle = (props: Props) => {
   return (
     <>
-      <Flex style={styles.container} direction='column' align='flex-start'>
+      <Flex style={styles.container} direction="column" align="flex-start">
         <h1 style={styles.title}>{props.label}</h1>
       </Flex>
     </>
-  )
-}
+  );
+};
 
 const styles: Styles = {
   container: {
-    width: '1000px',
-    textAlign: 'left',
-    background: '#fff'
+    width: "1000px",
+    textAlign: "left",
+    background: "#fff",
   },
   title: {
-    margin: '40px 20px 0 60px',
-    paddingLeft: '15px',
-    borderLeft: '10px solid #cdf'
+    margin: "40px 20px 0 60px",
+    paddingLeft: "15px",
+    borderLeft: "10px solid #2A2D55",
   },
-}
+};
 
-export default ContentTitle
+export default ContentTitle;
